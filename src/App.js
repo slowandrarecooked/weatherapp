548ae3daca3a93da423280525a4e1a097f673eb9
slowandrarecooked <airbendervineeth@gmail.com>
2023-12-11 20:10:36 +0530

    v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
 
+import axios from "axios";
 function App() {
+  const [data, setData] = useState([]);
+  const [input, setInput] = useState("");
+  const [location, setLocation] = useState("");
+  const [lat, setLat] = useState("");
+  const [long, setLong] = useState("");
+  const handleSubmit = async () => {
+    let data = await axios.get(
+      `https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=${lat}&lon=${long}&appid=6d448cd416719b471990d7aa0375c5a0`
+    );
+    console.log(data);
+  };
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
+  }, [lat, long]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather app</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter city name"
+          onChange={(e) => setLocation(e.target.value)}
+        />
+        <button onClick={handleSubmit}>Display Weather</button>
+      </div>
     </div>
   );
 }
