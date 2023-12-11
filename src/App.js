c838a4faee3c95a85d327cad63ab5bedbeafb3f8
slowandrarecooked <airbendervineeth@gmail.com>
2023-12-11 20:44:30 +0530

    v2

--- src/App.js
+++ src/App.js
@@ -6,21 +6,18 @@ import axios from "axios";
 function App() {
   const [data, setData] = useState([]);
   const [input, setInput] = useState("");
-  const [location, setLocation] = useState("");
-  const [lat, setLat] = useState("");
-  const [long, setLong] = useState("");
   const handleSubmit = async () => {
-    let data = await axios.get(
-      `https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=${lat}&lon=${long}&appid=6d448cd416719b471990d7aa0375c5a0`
+    let detes = await axios.get(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=5&appid=6d448cd416719b471990d7aa0375c5a0`
     );
-    console.log(data);
+
+    let weatherdetes = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${detes.data[0].lat}&lon=${detes.data[0].lon}&appid=6d448cd416719b471990d7aa0375c5a0`
+    );
+    console.log(weatherdetes);
+    setData(weatherdetes.data.main);
   };
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      setLat(position.coords.latitude);
-      setLong(position.coords.longitude);
-    });
-  }, [lat, long]);
+
   return (
     <div className="App">
       <h1>Weather app</h1>
@@ -28,9 +25,21 @@ function App() {
         <input
           type="text"
           placeholder="Enter city name"
-          onChange={(e) => setLocation(e.target.value)}
+          onChange={(e) => setInput(e.target.value)}
         />
         <button onClick={handleSubmit}>Display Weather</button>
+      </div>
+      <div>
+        {data ? (
+          <div>
+            <p>Temperature:{data.temp}</p>
+            <p>Feels Like:{data.feels_like}</p>
+            <p>Minimum Temperature:{data.temp_min}</p>
+            <p>Maximum Temperature:{data.temp_max}</p>
+            <p>Pressure:{data.pressure}</p>
+            <p>Humidity:{data.humidity}</p>
+          </div>
+        ) : null}
       </div>
     </div>
   );
